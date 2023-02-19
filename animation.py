54a2afb7ec1f511b400376a552e004b76c7a809e
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2023-02-19 16:33:41 +0300

    changed animation a bit

--- animation.py
+++ animation.py
@@ -2,26 +2,28 @@ import csv
 import matplotlib.pyplot as plt
 from celluloid import Camera
 
-fig = plt.figure()
-camera = Camera(fig)
-with open("output2.csv", 'r') as file:
-    csvreader = csv.reader(file, delimiter=';')
-    for row in csvreader:
-        row.pop(0)
-        dots = list(zip(row[::2], row[1::2]))
-        print(dots)
-        x = list()
-        y = list()
-        for num in dots:
-            x.append(float(num[0]))
-            y.append(float(num[1]))
-        print(x)
-        print(y)
-        plt.plot(x, y, 'ro')
-        plt.axis([-360, 100, -50, 50])  # вот здесь правим системы координат: максим и миним x y
-        camera.snap()
-        print()
-animation = camera.animate(repeat=True)
-plt.show()
-# в пайчарм оно не запустится просто так, нужно file -> settings -> tools -> python scientific убрать галочку
-# там где show plots что-то там
+
+def main():
+    fig = plt.figure()
+    camera = Camera(fig)
+    with open("output2.csv", 'r') as file:
+        csvreader = csv.reader(file, delimiter=';')
+        for row in csvreader:
+            row.pop(0)
+            dots = list(zip(row[::2], row[1::2]))
+            x = list()
+            y = list()
+            for num in dots:
+                x.append(float(num[0]))
+                y.append(float(num[1]))
+            plt.plot(x, y, 'ro')
+            plt.axis([min(x), max(x), min(y), max(y)])
+            camera.snap()
+    animation = camera.animate(repeat=True)
+    plt.show()
+    # в пайчарм оно не запустится просто так, нужно file -> settings -> tools -> python scientific убрать галочку
+    # там где show plots что-то там
+
+
+if __name__ == "__main__":
+    main()
